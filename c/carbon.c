f9ac4549bfa3f588dda9cb7fcfd47c4a5c2b6198
李枨煊 <chengxuan@staff.weibo.com>
2017-05-11 13:33:14 +0800

    完成：carbon写入数据封装

--- c/carbon.c
+++ c/carbon.c
@@ -15,62 +15,40 @@
 #include <netinet/in.h>
 #include <netdb.h>  /* netdb is necessary for struct hostent */
 
-#define PORT 4321  /* server port */
+#define PORT 2003  /* server port */
 
 #define MAXDATASIZE 100
 
 // 写入数据
-unsigned short carbon_write(char[] key, float value, unsigned int time) {
+short carbon_write(char *key[], float value, unsigned int time) {
+	int sockfd, num;
+	char buf[MAXDATASIZE];    /* buf will store received text */
+	struct hostent *he;
+	struct sockaddr_in server;
 
-}
-
-int main(int argc, char *argv[])
-{
-    int sockfd, num;    /* files descriptors */
-    char buf[MAXDATASIZE];    /* buf will store received text */
-    struct hostent *he;    /* structure that will get information about remote host */
-    struct sockaddr_in server;
-
-    if (argc != 2)
-    {
-        printf("Usage: %s <IP Address>\n",argv[0]);
-        exit(1);
+    if((sockfd=socket(AF_INET,SOCK_STREAM, 0))==-1) {
+        return -1;
     }
 
-    if((he=gethostbyname(argv[1]))==NULL)
-    {
-        printf("gethostbyname() error\n");
-        exit(1);
-    }
-
-    if((sockfd=socket(AF_INET,SOCK_STREAM, 0))==-1)
-    {
-        printf("socket() error\n");
-        exit(1);
-    }
     bzero(&server,sizeof(server));
-    server.sin_family = AF_INET;
-    server.sin_port = htons(PORT);
-    server.sin_addr = *((struct in_addr *)he->h_addr);
-    if(connect(sockfd, (struct sockaddr *)&server, sizeof(server))==-1)
-    {
-        printf("connect() error\n");
-        exit(1);
+	server.sin_family = AF_INET;
+	server.sin_port = htons(PORT);
+	server.sin_addr = *((struct in_addr *)he->h_addr);
+	if(connect(sockfd, (struct sockaddr *)&server, sizeof(server))==-1) {
+	   return -2;
+	}
+
+	char str[];
+	strcpy(str, key);
+	strcat(str, " ");
+	strcat(str, value);
+	strcat(str, " ");
+	stract(str, time);
+
+    if((num=send(sockfd,str,sizeof(str),0))==-1) {
+        return -3;
     }
 
-    char str[] = "horst\n";
-
-    if((num=send(sockfd,str,sizeof(str),0))==-1){
-        printf("send() error\n");
-        exit(1);
-    }
-    if((num=recv(sockfd,buf,MAXDATASIZE,0))==-1)
-    {
-        printf("recv() error\n");
-        exit(1);
-    }
     buf[num-1]='\0';
-    printf("server message: %s\n",buf);
-    close(sockfd);
-    return 0;
+    return 1;
 }
